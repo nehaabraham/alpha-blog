dfa8551532d54e50f99efe162b140b3d87172f96
Neha Abraham <me@nehabraham.com>
2016-12-27 09:30:25 -0500

    add styling to show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
 
     def destroy
       @article.destroy
-      flash[:notice] = "Article was successfully deleted"
+      flash[:danger] = "Article was successfully deleted"
       redirect_to articles_path
     end
 
